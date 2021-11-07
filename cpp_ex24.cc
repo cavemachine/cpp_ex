7937828f0a53fde3634f29b9db613306ad4cabb8
cavemachine <marcusprincipe@gmail.com>
2021-11-06 22:10:58 -0300

    commit 4

--- cpp_ex24.cc
+++ cpp_ex24.cc
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+#define PI 3.14159265
 
 int main () {
 
-  
+  int altura = 19, comprimento = 9, largura = 6, angulo, volume;
+  // cout << "altura: ";
+  // cin >> altura;
+  // cout << "comprimento: ";
+  // cin >> comprimento;
+  // cout << "largura: ";
+  // cin >> largura;
+  cout << "angulo: ";
+  cin >> angulo;
+
+  int vol_triang, altura_triang, compr_triang, larg_triang;
+  compr_triang = comprimento;
+  larg_triang = largura;
+  altura_triang = tan((angulo * PI) / 180)*comprimento;
+
+  vol_triang = (altura_triang * compr_triang * larg_triang) / 2;
+  volume = (altura*comprimento*largura);
+  cout << "volume da caixa: " << volume << endl;
+  cout << "volume do triang: " << vol_triang << endl;
+  cout << "restante na caixa: " << volume - vol_triang << endl;
   
   return 0;
 }
+// http://web.kshs.kh.edu.tw/academy/luckycat/homework/q11909.htm
+// <<incompleto>>
